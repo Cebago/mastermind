b9942a571fd14c7df3d46643e44b820933475852
DEMARCHE Baptiste <bdemarche@myges.fr>
2021-10-26 22:21:30 +0200

    some update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// Author: batmine3
+// Date: 26/10/2021
+// Subject: Make the famous mastermind game
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world")    
+}
+
+fn user_entry() -> String {
+    return "Coucou".to_string();
+}
+
+fn make_correction() -> String {
+    return "_XO".to_string();
 }
